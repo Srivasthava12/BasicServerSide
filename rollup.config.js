13340bdc6ae7cdbd857f6bca3422021e56c6a1d2
srivasthava12 <srivasthavatime@gamil.com>
2021-03-13 21:05:36 +0530

    mobile view class name changes

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,27 @@
-import babel from '@rollup/plugin-babel';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import replace from '@rollup/plugin-replace';
+import babel from "@rollup/plugin-babel";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
 
 export default {
-  input: 'src/index.js',
+  input: "src/index.js",
   output: {
-    file: 'public/dist/index.js',
-    format: 'es',
-    sourcemap: true
+    file: "public/dist/index.js",
+    format: "es",
+    sourcemap: true,
   },
   plugins: [
     replace({
-      'process.env.NODE_ENV': JSON.stringify('development')
+      "process.env.NODE_ENV": JSON.stringify("development"),
     }),
     nodeResolve({
-      extensions: ['.js', '.jsx']
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
     }),
     babel({
-      presets: ['@babel/preset-react']
+      presets: ["@babel/preset-react"],
     }),
     commonjs({
-      include: ['node_modules/**']
+      include: ["node_modules/**"],
     }),
-  ]
+  ],
 };
--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
 /** @jsx h */
-import { h, Component, render } from 'preact';
-import { Router } from 'preact-router';
-import Todo from './pages/todo/todo'
-import Home from './pages/home/home'
+import { h, Component, render } from "preact";
+import { Router } from "preact-router";
+import Todo from "./pages/todo/todo";
+import Home from "./pages/home/home";
 
 class App extends Component {
-
-    render() {
-        return (
-            <div class="app">
-                <Router>
-                    <Home path="/" page="All" />
-                    <Todo path="/todo" page="All" />
-                </Router>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div class="container is-mobile">
+        <Router>
+          <Home path="/" page="All" />
+          <Todo path="/todo" page="All" />
+        </Router>
+      </div>
+    );
+  }
 }
 
-
-
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
